3aa2f88f0e6edabc2c3ae5e59ba99a5bc803cd45
rahuraman <nrahuraman@yahoo.com>
2024-03-26 22:36:17 +0530

    intialCommit- intial setup done!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { PlaylistComponent } from './pages/playlist/playlist.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginpageComponent } from './pages/loginpage/loginpage.component';
+import { SongCardComponent } from './components/song-card/song-card.component';
+import { NavigationCardComponent } from './components/navigation-card/navigation-card.component';
+import { CustomButtonComponent } from './components/custom-button/custom-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    PlaylistComponent,
+    SignupComponent,
+    LoginpageComponent,
+    SongCardComponent,
+    NavigationCardComponent,
+    CustomButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/navigation-card/navigation-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-card',
+  templateUrl: './navigation-card.component.html',
+  styleUrl: './navigation-card.component.css'
+})
+export class NavigationCardComponent {
+
+}
